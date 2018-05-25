9a497cdf2512a67c78413061eed906f0bdbed33a
unknown <ywang7@hzad1.com>
2018-05-25 20:09:09 +0800

    update

--- pages/frontend/index.js
+++ pages/frontend/index.js
@@ -1,9 +1,38 @@
+import { Table } from 'antd'
 import { axios } from '../../config/common'
 
 const Index = (props) => {
   const list = props.list.map((item, index) => <li key={index}>{item.Title}</li>);
+  const dataSource = [{
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号'
+  }, {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号'
+  }];
+  
+  const columns = [{
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  }, {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  }, {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  }];
   return (
-    <ul>{list}</ul>
+    <div>
+      <Table dataSource={dataSource} columns={columns} />
+      <ul>{list}</ul>
+    </div>
   )
 };
 
