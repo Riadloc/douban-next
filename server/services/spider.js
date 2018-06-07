7b29634ac0f28e6a94efa8f67b4e8bf0cd741581
unknown <ywang7@hzad1.com>
2018-06-07 11:52:11 +0800

    update

--- server/services/spider.js
+++ server/services/spider.js
@@ -33,53 +33,58 @@ module.exports = {
     const page_detail = await browser.newPage();
     while (flag) {
       console.log(`..........开始爬取${start/25 + 1}页.........`)
-      await page.goto(GROUP_RENTING + 'start='+ start);
-      await page.waitForSelector('.olt');
-      const html = await page.$$eval('.olt tr', list => {
-        return list.slice(1).map(item => {
-          const aTag = $(item).find('.title a');
-          const [ user, reply, update_time ] = item.innerText.split('\t').slice(1);
-          const link = aTag.attr('href');
-          const temp = link.split('/');
-          const id = temp[temp.indexOf('topic')+1]
-          return { id, title: aTag.attr('title'), user, reply, update_time, link };
-        })
-      });
-      for (const value of html) {
-        let { update_time, link } = value;
-        update_time = update_time.split(' ')[0];
-        if (!timeReg.test(update_time)) update_time = `${currentYear}-${update_time}`;
-        if (dayjs(updateTimeBegin).isAfter(dayjs(update_time))) {
-          flag = false;
-          break;
-        }
-        await page_detail.goto(link);
-        delete value['link'];
-        await page_detail.waitForSelector('.topic-content', { timeout: 1000 }).then(async () => {
-          const detail = await page_detail.$eval('.topic-content', content => {
-            const avatar = $(content).find('.user-face img').attr('src').split('/').slice(-1)[0];
-            const create_time = $(content).find('.topic-doc .color-green').text();
-            const description = $(content).find('#link-report .topic-richtext p').text() || '';
-            const detail_imgs = [];
-            $(content).find('#link-report .topic-richtext img').each((idx, x) => {
-              detail_imgs.push($(x).attr('src').split('/').slice(-1)[0]);
-            })
-            return { avatar, description, create_time, detail_imgs: JSON.stringify(detail_imgs) }
+      await page.goto(GROUP_RENTING + 'start='+ start).then(async () => {
+        await page.waitForSelector('.olt');
+        const html = await page.$$eval('.olt tr', list => {
+          return list.slice(1).map(item => {
+            const aTag = $(item).find('.title a');
+            const [ user, reply, update_time ] = item.innerText.split('\t').slice(1);
+            const link = aTag.attr('href');
+            const temp = link.split('/');
+            const id = temp[temp.indexOf('topic')+1]
+            return { id, title: aTag.attr('title'), user, reply, update_time, link };
           })
-          list.push(Object.assign(detail, value));
-          await page_detail.waitFor(1500);
-        }).catch(() => {
-          console.log(value.id + ' failed');
-        })
-      }
-      console.log(`..........爬取${start/25 + 1}页完成.........`)
-      start = start + offset;
-      await page.waitFor(1500);
+        });
+        for (const value of html) {
+          let { update_time, link } = value;
+          update_time = update_time.split(' ')[0];
+          if (!timeReg.test(update_time)) update_time = `${currentYear}-${update_time}`;
+          if (dayjs(updateTimeBegin).isAfter(dayjs(update_time))) {
+            flag = false;
+            break;
+          }
+          await page_detail.goto(link).then(async () => {
+            delete value['link'];
+            await page_detail.waitForSelector('.topic-content', { timeout: 1000 }).then(async () => {
+              const detail = await page_detail.$eval('.topic-content', content => {
+                const avatar = $(content).find('.user-face img').attr('src').split('/').slice(-1)[0];
+                const create_time = $(content).find('.topic-doc .color-green').text();
+                const description = $(content).find('#link-report .topic-richtext p').text() || '';
+                const detail_imgs = [];
+                $(content).find('#link-report .topic-richtext img').each((idx, x) => {
+                  detail_imgs.push($(x).attr('src').split('/').slice(-1)[0]);
+                })
+                return { avatar, description, create_time, detail_imgs: JSON.stringify(detail_imgs) }
+              })
+              list.push(Object.assign(detail, value));
+              await page_detail.waitFor(1500);
+            }).catch(() => {
+              console.log(value.id + ' failed');
+            })
+          }).catch(() => {
+            console.log('something failed');
+          })
+        }
+        console.log(`..........爬取${start/25 + 1}页完成.........`)
+        start = start + offset;
+        await page.waitFor(1500);
+      }).catch((err) => {
+        console.log((start/25+1) + '页 failed');
+      })
     }
     await browser.close();
     const user_list_arr = list.map(item => item.user);
     const user_list_str = ',' + user_list_arr.join(',') + ',';
-    const user_list_set = [...new Set(user_list_arr)];
     list.forEach(async (item) => {
       let { create_time, update_time, title, user, description } = item;
       create_time = dayjs(create_time.split(' ')[0]);
